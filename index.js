f8d57cbc61b3cae181e264f4a8ed3d4bb2546e50
Ayush Sachdeva <ayush000@gmail.com>
2019-12-20 22:38:04 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const fetch = require("node-fetch");
+const Telegram = require("node-telegram-bot-api");
+const bot = new Telegram(process.env.TELEGRAM_TOKEN);
+
+const weatherToken = process.env.WEATHER_API_TOKEN;
+
+const weatherURL = new URL("https://api.openweathermap.org/data/2.5/weather");
+console.log(weatherToken);
+
+weatherURL.searchParams.set("APPID", weatherToken);
+weatherURL.searchParams.set("zip", "411014,india");
+weatherURL.searchParams.set("units", "imperial");
+
+const getWeatherData = async () => {
+  const resp = await fetch(weatherURL.toString());
+  const body = await resp.json();
+  return body;
+};
+
+const generateWeatherMessage = weatherData => `The weather in ${weatherData.name}:
+${weatherData.weather.description}. Current temperature is ${weatherData.main.temp} with a low of
+temp ${weatherData.main.temp_min} and a high of ${weatherData.main.temp_max}`;
+
+const main = async () => {
+  const weatherData = await getWeatherData();
+  const weatherString = generateWeatherMessage(weatherData);
+  bot.sendMessage(process.env.TELEGRAM_CHAT_ID, weatherString);
+};
+
+main();
